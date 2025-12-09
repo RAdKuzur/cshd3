53b4dbfccb9f05b947632b561990680aaa84a8cf
kuzurgalievra <kuzurgalievra@teletribe.ru>
2025-12-10 01:17:13 +0400

    fix deploy v2

--- start.sh
+++ start.sh
@@ -16,10 +16,10 @@ docker compose exec backend composer install
 docker compose exec backend php artisan migrate
 
 echo "Выдача прав на запись логов для laravel"
-docker compose exec -it backend chown -R www-data:www-data /var/www/html/storage
-docker compose exec -it backend chmod -R 775 /var/www/html/storage
-docker compose exec -it backend chown -R www-data:www-data /var/www/html/bootstrap/cache
-docker compose exec -it backend chmod -R 775 /var/www/html/bootstrap/cache
+#docker compose exec -it backend chown -R www-data:www-data /var/www/html/storage
+#docker compose exec -it backend chmod -R 775 /var/www/html/storage
+#docker compose exec -it backend chown -R www-data:www-data /var/www/html/bootstrap/cache
+#docker compose exec -it backend chmod -R 775 /var/www/html/bootstrap/cache
 
 docker compose exec backend php artisan jwt:secret
 
