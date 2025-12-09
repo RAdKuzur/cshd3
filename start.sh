4e256efd748ccb1e5bca09b5507cd86f50614e86
kuzurgalievra <kuzurgalievra@teletribe.ru>
2025-12-10 01:13:18 +0400

    deploy fix

--- start.sh
+++ start.sh
@@ -9,11 +9,18 @@ cd frontend && npm install
 echo "Устанавливаем бэкенд зависимости..."
 cd ../backend && cp .env.example .env
 
-echo "Установка завершена! Запускать Docker..."
+echo "Установка завершена! Запускаем Docker..."
 
 cd .. && docker compose up --build -d
 docker compose exec backend composer install
 docker compose exec backend php artisan migrate
+
+echo "Выдача прав на запись логов для laravel"
+docker compose exec -it backend chown -R www-data:www-data /var/www/html/storage
+docker compose exec -it backend chmod -R 775 /var/www/html/storage
+docker compose exec -it backend chown -R www-data:www-data /var/www/html/bootstrap/cache
+docker compose exec -it backend chmod -R 775 /var/www/html/bootstrap/cache
+
 docker compose exec backend php artisan jwt:secret
 
 echo "Проект запущен"
