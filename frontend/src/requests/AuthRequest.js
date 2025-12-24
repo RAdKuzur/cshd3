2275604a640fe62fd21e5736b8deb99e5bfd62e8
EscBar <timuryyaga@gmail.com>
2025-12-24 12:33:47 +0400

    Refresh absolute uri fix

--- frontend/src/requests/AuthRequest.js
+++ frontend/src/requests/AuthRequest.js
@@ -5,17 +5,17 @@ import { BACKEND_URL } from '@/router.js'
 axios.defaults.withCredentials = true
 
 export async function loginApi(email, password) {
-    const res = await axios.post(`${BACKEND_URL}api/login`, { email, password })
+    const res = await axios.post(`${BACKEND_URL}/api/login`, { email, password })
     return res.data
 }
 
 export async function logoutApi() {
     Cookies.remove('access_token')
     Cookies.remove('refresh_token')
-    return axios.post(`${BACKEND_URL}api/logout`)
+    return axios.post(`${BACKEND_URL}/api/logout`)
 }
 
 export async function refreshApi() {
-    const res = await axios.post(`${BACKEND_URL}api/refresh`)
+    const res = await axios.post(`${BACKEND_URL}/api/refresh`)
     return res.data
 }
