5d05918d253e00da946b324eb1bea575d286a614
kuzurgalievra <kuzurgalievra@teletribe.ru>
2026-01-31 17:37:04 +0400

    network-things search

--- backend/app/Helpers/UrlHelper.php
+++ backend/app/Helpers/UrlHelper.php
@@ -6,6 +6,18 @@ class UrlHelper
 {
     public static function createUrlFromSearch($index, $id)
     {
-        return env('FRONTEND_URL', 'http://localhost') . '/' . $index . '/view/' . $id;
+        return env('FRONTEND_URL', 'http://localhost') . '/' . self::map($index) . '/view/' . $id;
+    }
+
+    public static function map($index)
+    {
+        switch ($index) {
+            case 'things':
+                return 'things';
+            case 'network-things':
+                return 'things/network';
+            default:
+                return null;
+        }
     }
 }
--- backend/database/seeders/NetworkThingSeeder.php
+++ backend/database/seeders/NetworkThingSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Dictionaries\ThingTypeDictionary;
+use App\Services\ElasticsearchService;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -12,34 +13,56 @@ class NetworkThingSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+    private ElasticsearchService $elasticsearchService;
+    public function __construct(
+        ElasticsearchService $elasticsearchService
+    )
+    {
+        $this->elasticsearchService = $elasticsearchService;
+    }
     public function run(): void
     {
+        $this->elasticsearchService->create('network-things');
+
         DB::table('network_things')->truncate();
         $networkThings = DB::table('things')->whereIn('thing_type_id', [
             ThingTypeDictionary::PC, ThingTypeDictionary::ARM, ThingTypeDictionary::FAX, ThingTypeDictionary::KMA,
             ThingTypeDictionary::PRINTER, ThingTypeDictionary::SERVER, ThingTypeDictionary::NETWORK_BORDER, ThingTypeDictionary::MONOPC,
             ThingTypeDictionary::STORAGE
         ])->get();
-
         foreach ($networkThings as $networkThing) {
-            DB::table('network_things')->insert([
+            $networkThingsId = DB::table('network_things')->insertGetId([
                 'thing_id' => $networkThing->id,
                 'ip_address' => fake()->ipv4(),
                 'phone_number' => null,
-                'comment' => fake()->text()
+                'comment' => $fakeText = fake()->text()
             ]);
+            $this->elasticsearchService->index(
+                'network-things',
+                [
+                    'comment' => $fakeText,
+                    'id' => $networkThingsId
+                ]
+            );
         }
+
         $telephones = DB::table('things')->whereIn('thing_type_id', [
             ThingTypeDictionary::TELEPHONE
         ])->get();
         foreach ($telephones as $telephone) {
-            DB::table('network_things')->insert([
+            $networkThingsId = DB::table('network_things')->insertGetId([
                 'thing_id' => $telephone->id,
                 'ip_address' => null,
                 'phone_number' => fake()->phoneNumber(),
-                'comment' => fake()->text()
+                'comment' => $fakeText = fake()->text()
             ]);
+            $this->elasticsearchService->index(
+                'network-things',
+                [
+                    'comment' => $fakeText,
+                    'id' => $networkThingsId
+                ]
+            );
         }
-
     }
 }
