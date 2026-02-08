d6178158584b1a26542a3b0722fa1c17e69fc294
kuzurgalievra <kuzurgalievra@teletribe.ru>
2026-02-08 11:19:21 +0400

    licence check update

--- backend/app/Console/Commands/LicenceCheckCommand.php
+++ backend/app/Console/Commands/LicenceCheckCommand.php
@@ -7,6 +7,7 @@ use App\Services\LicenceService;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use function Psy\debug;
 
 class LicenceCheckCommand extends Command
 {
@@ -29,32 +30,48 @@ class LicenceCheckCommand extends Command
      */
     public function handle()
     {
+        $url = env('LICENCE_URL_APP'); # путь к лицензии
+        $licenceKeys = DB::table('licences')->get();
+        $key = env('APP_KEY'); # ключ приложения Laravel
         DB::beginTransaction();
         try {
-            $url = env('LICENCE_URL_APP');
-            $licenceKeys = DB::table('licences')->get();
-            $key = env('APP_KEY');
             foreach ($licenceKeys as $licenceKey) {
-            if ($licenceKey && $url) {
-                $response = Http::post($url, [
-                    'app_key' => $key,
-                    'licence_key' => $licenceKey->code,
-                ]);
-                switch ($response->status()) {
-                    case 200:
-                    case 403:
+                if ($licenceKey && $url) {
+                    try {
+                        $response = Http::post($url, [
+                            'app_key' => $key,
+                            'licence_key' => $licenceKey->code,
+                        ]);
+                        switch ($response->status()) {
+                            case 200:
+                            case 403:
+                                DB::table('licences')->where(['id' => $licenceKey->id])->update(
+                                    [
+                                        'is_revoked' => $response->json()['data']['is_revoked'],
+                                        'expires_at' => $response->json()['data']['expires_at']
+                                    ]
+                                );
+                                break;
+                            case 404:
+                                DB::table('licences')->where(['id' => $licenceKey->id])->delete();
+                                break;
+                            default:
+                                DB::table('licences')->where(['id' => $licenceKey->id])->update(
+                                    [
+                                        'is_revoked' => LicenceDictionary::REVOKED,
+                                    ]
+                                );
+                                break;
+                        }
+                    }
+                    catch (\Exception $e) {
                         DB::table('licences')->where(['id' => $licenceKey->id])->update(
                             [
-                                'is_revoked' => $response->json()['data']['is_revoked'],
-                                'expires_at' => $response->json()['data']['expires_at']
+                                'is_revoked' => LicenceDictionary::REVOKED,
                             ]
                         );
-                        break;
-                    case 404:
-                        DB::table('licences')->where(['id' => $licenceKey->id])->delete();
-                        break;
+                    }
                 }
-            }
             }
             DB::commit();
         }
